fixed learning rate and bypassed weight computation

scripts/demo-SBM-CM-family/SBM-CM-family.py
@@ -18,7 +18,7 @@ results_dir = ROOT / "results"
 
 ##########################################################
 
-def run_SBM(Input_MSA,Weights, fam,Model,train_file,N_iter, m, N_chains_list,Nb_rep,Nb_av,k_MCMC,TestTrain,ParamInit,lambdJ,lambdh):
+def run_SBM(Input_MSA,Weights, fam,Model,train_file,N_iter, m, N_chains_list,Nb_rep,Nb_av,k_MCMC,TestTrain,ParamInit,lambdJ,lambdh,LearningRate):
     fam = str(fam)
     
     for rep in range(Nb_rep):
@@ -43,8 +43,9 @@ def run_SBM(Input_MSA,Weights, fam,Model,train_file,N_iter, m, N_chains_list,Nb_
                                 ('lambda_h', lambdh), ('lambda_J', lambdJ),
                                 ('Pruning', False), ('Pruning Mask', None),
                                 ('Param_init', ParamInit),
+                                ('LearningRate' , LearningRate),
                                 ('Test/Train', TestTrain==1), ('Train sequences', ind_train),
-                                ('Weights', np.load(str(Weights))), ('SGD', None),
+                                ('Weights', None), ('SGD', None),
                                 ('Seed', None), ('Zero Fields', False), 
                                 ('Store Parameters', None)])
 
@@ -128,10 +129,12 @@ if __name__ == "__main__":
     parser.add_argument('--lambdh', type=float, default=0, help='lambda h')
     parser.add_argument('--Weights')
     parser.add_argument('--Input_MSA')
+    parser.add_argument('--LearningRate' , type=float, default=1e-3, help = 'Fixed Learning Rate')
+
 
     args = parser.parse_args()
     run_SBM(args.Input_MSA, args.Weights , args.fam,args.mod,args.train_file,args.N_iter, 
             args.m, args.N_chains,args.rep,args.N_av,args.k_MCMC,args.TestTrain,
-            args.ParamInit,args.lambdJ,args.lambdh)
+            args.ParamInit,args.lambdJ,args.lambdh,args.LearningRate)
     
 

src/SBM/SBM_GD/SBM_proteins.py
@@ -28,7 +28,7 @@ def ParseOptions(options):
         ('PseudoCount',False), # the default pseudo count is 1/Neff
 
         ('alpha',0.2),  #Learning rate for the BM method
-        ('Learning_rate',None),
+        ('LearningRate',None),
 
         ('lambda_h', 0),   # regularization for the fields
         ('lambda_J', 0),    # regularization for the couplings
@@ -40,7 +40,7 @@ def ParseOptions(options):
         ('Pruning Mask Couplings', None),
         ('Infinite Mask Fields',None), # To forbid certain a.a at certain positions
         
-        ('Param_init', 'profile'), # Zero, Profile, Custom
+        ('Param_init', 'zero'), # Zero, Profile, Custom
 
         ('Test/Train', True), #If True and 'Train sequences' is None: the MSA is randomly splitted in a 80% training set / 20% test set
         ('Train sequences',None), #indices of sequences used for training
@@ -129,11 +129,15 @@ def Init_SGD(options,train_align):
     return align_subsamp
 
 def Init_statistics(options,train_align):
-
+    '''
+    In the CONF version of the Boltzmann Machine we don't want to compute weights
+    as the equivalent of 'phylogeny is exactly what we want to preserve when studying conformational changes.
+    '''
     ###### EVALUATE GOAL STATS #####
     print('Compute the statistics from the database....')
     if options['Weights'] is None:
-        W,N_eff=ut.CalcWeights(train_align,options['theta'])
+        W = np.ones(train_align.shape[0])
+        N_eff = train_align.shape[0]
     else:
         print(len(options['Weights']))
         print(train_align.shape[0])
@@ -237,6 +241,8 @@ def SBM(align,options,J0 = None,h0 = None):
     f=lambda x: GradLogLike(x,lamJ,lamh,fi,fij,options,align_subsamp=align_subsamp)
     ################################
     
+    print('about to start GD...')
+
     ####### GRADIENT DESCENT #######
     Ex_time=time.time()
     w,output=Minimizer(f,w0,options)
@@ -300,6 +306,7 @@ def GradLogLike(w,lambdaJ,lambdah,fi,fij,options,align_subsamp=None):
     if options['Zero Couplings']:gradJ=None
 
     grad=ut.Wj(gradJ,gradh)
+    print(np.linalg.norm(grad))
     return grad
 
 ##########################################################
@@ -332,8 +339,7 @@ def Minimizer(fun,x0,options):
         
         ########## BM METHOD ###########
         else:
-            if options['Learning_rate'] is not None: t = options['Learning_rate'][i]
-            else:t = 1/((i+1)**options['alpha'])
+            if options['LearningRate'] is not None: t = options['LearningRate']
             grad = fun(x)
             x -= t*grad
         ################################

src/SBM/utils/utils.py
@@ -236,39 +236,53 @@ def CalcWeights(align,theta):
 	N_eff=sum(W)
 	return W,N_eff
 
-def CalcStatsWeighted(q,MSA,p=None):
-	"""
-	Function to calculate the statistics of a given weighted multiple sequence alignment,
-	including the frequencies and pairwise frequencies.
+def CalcStatsWeighted(q, MSA, p=None):
+    """
+    Function to calculate the statistics of a given weighted multiple sequence alignment,
+    including the frequencies and pairwise frequencies.
 
-	Parameters:
-	q : int
-		The number of amino acids.
-	MSA : numpy array
-		A 2D numpy array representing the Multiple Sequence Alignment.
-	p : numpy array
-		An array representing the weights. If None, it is set to an array of equal weights.
+    Parameters:
+    q : int
+        The number of amino acids.
+    MSA : numpy array
+        A 2D numpy array representing the Multiple Sequence Alignment.
+    p : numpy array
+        An array representing the weights. If None, it is set to an array of equal weights.
 
-	Returns:
-	fi : numpy array
-		A 2D numpy array with the calculated frequencies.
-	fij : numpy array
-		A 4D numpy array with the pairwise frequencies.
-	"""
-	if p is None:
-		p= np.zeros(MSA.shape[0])+1/MSA.shape[0]
-	L=MSA.shape[1]  
-	fi=np.zeros([L,q])
-	x=np.array([i for i in range(L)])
-	for m in range(MSA.shape[0]):
-		fi[x[:],MSA[m,x[:]]]+= p[m]
+    Returns:
+    fi : numpy array
+        A 2D numpy array with the calculated frequencies.
+    fij : numpy array
+        A 4D numpy array with the pairwise frequencies.
+    """
+    if p is None:
+        p = np.zeros(MSA.shape[0]) + 1 / MSA.shape[0]
 
-	fij=np.zeros([L,L,q,q])
-	x=np.array([[i,j] for i,j in it.product(range(L),range(L))])
-			
-	for m in range(MSA.shape[0]):
-		fij[x[:,0],x[:,1],MSA[m,x[:,0]],MSA[m,x[:,1]]]+=p[m]
-	return fi,fij
+    L = MSA.shape[1]
+    fi = np.zeros([L, q])
+    x = np.array([i for i in range(L)])
+
+    total_seq = MSA.shape[0]
+    print("Computing single-site frequencies...")
+    for m in range(total_seq):
+        fi[x[:], MSA[m, x[:]]] += p[m]
+        if (m + 1) % max(1, total_seq // 100) == 0 or (m + 1) == total_seq:
+            pct = (m + 1) / total_seq * 100
+            print(f"\r  Progress: {pct:.1f}% ({m + 1}/{total_seq} sequences)", end="", flush=True)
+    print()  # newline after progress
+
+    fij = np.zeros([L, L, q, q])
+    x = np.array([[i, j] for i, j in it.product(range(L), range(L))])
+
+    print("Computing pairwise frequencies...")
+    for m in range(total_seq):
+        fij[x[:, 0], x[:, 1], MSA[m, x[:, 0]], MSA[m, x[:, 1]]] += p[m]
+        if (m + 1) % max(1, total_seq // 100) == 0 or (m + 1) == total_seq:
+            pct = (m + 1) / total_seq * 100
+            print(f"\r  Progress: {pct:.1f}% ({m + 1}/{total_seq} sequences)", end="", flush=True)
+    print()  # newline after progress
+
+    return fi, fij
 
 def CalcThreeCorrWeighted(MSA,fi,fij,p=None,ind_L = None):
 	"""